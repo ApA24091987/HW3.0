5f02b8b45e4657ed6de0a53b5f4b6170d83d2205
ApA24091987 <168029434+ApA24091987@users.noreply.github.com>
2024-05-01 20:40:22 +0300

    Add files via upload

---
+++ yuri.py
@@ -0,0 +1,8 @@
+name="Yuri"
+print('name:Yuri', type(name))
+age=36
+print('age:36', type(age))
+new_age=37
+print('new_age:37', type(new_age))
+is_student='True'
+print('is_student:True', type('True'))
